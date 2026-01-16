81a365730236df6afe90541016632659a780c192
Matt Thompson <mattthompson56@gmail.com>
2026-01-16 03:17:53 -0500

    Still working on breadth first traversal method. Found info on medium I need to read up on

--- lib/bst.rb
+++ lib/bst.rb
@@ -26,7 +26,6 @@ class Tree
 
     mid = idx_start + (idx_end - idx_start) / 2
     self.root = Node.new(arr[mid])
-    #puts root.data
 
     self.root.left = build_tree(arr, idx_start, mid - 1)
     self.root.right = build_tree(arr, mid + 1, idx_end)
@@ -45,31 +44,21 @@ class Tree
     end
   end
 
-  def traversal(node = root, &block)
-    return nil if node.nil? 
-    queue = [node]
-    q_len = queue.length - 1
-    queue.each_with_index do |node, idx| 
-      block.call(node) if block_given? 
-      unless node.class == Node
-        queue << node.left unless node.left.nil?
-        queue << node.right unless node.right.nil? 
-      end
-      break if idx == q_len
-    end
-    traversal(queue, &block)
+#node = root, 
+  def traversal(queue = [root], &block)
+
+    block.call(queue) if block_given?
+    queue.each_with_index do |node, index|
+      queue << node.left unless node.left.nil? 
+      queue << node.right unless node.right.nil?
+    end 
   end
 end
-      
-
-
+#    q_len = queue.length - 1
 arr = Array.new(50) { rand(1..100) }
-
+arr.sort!.uniq!
 bst = Tree.new(arr, 0, arr.length - 1)
-
-puts bst.root.nil?
-bst.traversal { |node| puts node }
-
+bst.traversal { |queue| puts queue[1].data } 
 
 
 #  def tree_height(current = root, l_height = 0, r_height = 0)
